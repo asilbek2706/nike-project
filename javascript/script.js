667e3bfc9dd38c887438f5b4cf40f5d21d9f52f9
Asilbek2706 <249135167+Asilbek2706@users.noreply.github.com>
2026-04-18 10:29:09 +0500

    Project done

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,15 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const menuIcon = document.querySelector('.menu-icon'),
-    mobileMenu = document.querySelector('.mobile-menu'),
+const menuIcon = document.querySelector('.menu-icon'),
+  mobileMenu = document.querySelector('.mobile-menu'),
+  html = document.querySelector('html');
 
-  function toggleMobileMenu() {
-    mobileMenu.classList.toggle('active');
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle('active');
+
+  if (mobileMenu.classList.contains('active')) {
+    html.style.overflow = 'hidden';
+  } else {
+    html.style.overflow = 'scroll';
   }
+}
 
-
-  menuIcon.addEventListener('click', toggleMobileMenu);
-});
+menuIcon.addEventListener('click', toggleMobileMenu);
